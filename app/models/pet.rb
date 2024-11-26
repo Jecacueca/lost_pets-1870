b08345add4962ab19f82a1da836aa46fac96b0b5
Jess Silva Carvalho <jc.carvalho@outlook.com>
2024-11-26 16:54:13 +0000

    CRUD for our lost pets done

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,6 @@
 class Pet < ApplicationRecord
+  SPECIES = %w(dog cat bird ferret turtle rabbit pony)
+  
+  validates :name, presence: true
+  validates :species, inclusion: { in: Pet::SPECIES }
 end
